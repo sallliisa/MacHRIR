3c7a3f09e25e4630b103a859ce22b2693ae6f554
sallliisa <sodiumcation@gmail.com>
2025-11-19 00:00:14 +0700

    Initial Commit

---
+++ MacHRIR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacHRIR
+//
+//  Created by gamer on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacHRIR/MacHRIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacHRIRApp.swift
+//  MacHRIR
+//
+//  Created by gamer on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MacHRIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
